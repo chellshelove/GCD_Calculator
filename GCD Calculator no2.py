9e191bb494e63d442f7c03c77c471e11836b820a
chellshelove <chellshelove733@gmail.com>
2022-01-31 09:57:00 +0700

    final code

--- GCD Calculator no2.py
+++ GCD Calculator no2.py
@@ -1,18 +1,13 @@
+# method to compute gcd ( Euclidean algo )
+
 def computeGCD(x, y):
-  
-    if x > y:
-        small = y
-    else:
-        small = x
-    for i in range(1, small+1):
-        if((x % i == 0) and (y % i == 0)):
-            gcd = i
-              
-    return gcd
-  
+    while(y):
+        x, y = y, x % y
+    return x
+
 a = 1274
 b = 10505
-  
+
 # prints 12
 print ("The gcd of a and b is : ",end="")
 print (computeGCD(1274,10505))
